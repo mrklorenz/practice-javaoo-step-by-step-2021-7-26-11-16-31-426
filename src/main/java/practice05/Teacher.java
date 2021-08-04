7dfed0ba7bc3263bf647ebb9f679b8aebe6f778c
balajma <mark.lorenz.balajadia@oocl.com>
2021-08-04 18:54:02 +0800

    [OOP][Mark] feat: Added methods and attributes in Teacher.java

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,41 @@
 package practice05;
 
-public class Teacher {
+public class Teacher extends Person {
+
+
+    private String name;
+    private int age;
+    private Integer klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.name = name;
+        this.age = age;
+        this.klass = klass;
+    }
+
+    public String introduce(){
+        if (this.klass == null) {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        }
+        return super.introduce() + " I am a Teacher. I teach Class " + this.klass + ".";
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
 }
