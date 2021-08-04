67c98fcab4184c76d27b8adeac14cadd20b12a17
balajma <mark.lorenz.balajadia@oocl.com>
2021-08-05 00:23:22 +0800

    [OOP][Mark] feat: Added methods and attributes in Person.java

--- src/main/java/practice08/Person.java
+++ src/main/java/practice08/Person.java
@@ -1,4 +1,27 @@
 package practice08;
 
 public class Person {
+
+    private int id;
+
+    private String name;
+    private int age;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce(){
+        return "My name is " + this.name + ". I am " + this.age + " years old.";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
