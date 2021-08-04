e040fb70f401b6f4bc9b2baec7dc52a28e1b018d
balajma <mark.lorenz.balajadia@oocl.com>
2021-08-04 16:35:24 +0800

    [OOP][Mark] feat: Added methods and attributes in Worker.java

--- src/main/java/practice03/Worker.java
+++ src/main/java/practice03/Worker.java
@@ -1,4 +1,29 @@
 package practice03;
 
-public class Worker {
+public class Worker extends Person{
+    private String name;
+    private int age;
+
+    public Worker(String name, int age){
+        super(name, age);
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce(){
+        return "I am a Worker. I have a job.";
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+
+
+
 }
