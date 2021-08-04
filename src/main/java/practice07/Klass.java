15eb8da2eb442be4200fb9a15f9154ef53014339
balajma <mark.lorenz.balajadia@oocl.com>
2021-08-04 21:27:53 +0800

    [OOP][Mark] feat: Added methods and attributes to Klass.java

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,28 @@
 package practice07;
 
 public class Klass {
+
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + this.number;
+    }
+
+    public Student assignLeader(Student student) {
+        this.leader = student;
+        return this.leader;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
 }
