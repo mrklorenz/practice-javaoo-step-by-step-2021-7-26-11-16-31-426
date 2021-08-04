777a40521f0f42a17e8a2c2f5c217f98474b02f6
balajma <mark.lorenz.balajadia@oocl.com>
2021-08-04 17:09:58 +0800

    [OOP][Mark] feat: Added method and attributes in Student.java

--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -1,4 +1,33 @@
 package practice04;
 
-public class Student {
+public class Student extends Person{
+
+    private String name;
+    private int age;
+    private int klass;
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.name = name;
+        this.age = age;
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce(){
+        return super.introduce() + " I am a Student. I am at Class " + this.klass + ".";
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
 }
